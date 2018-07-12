803c4d9f9ee6fdd6ad25921b4ae83416ef52eeb9
Christopher Silich <chrissilich@gmail.com>
2018-07-12 10:42:51 -0400

    ready!

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,18 @@
 "use strict";
 
 console.log("Hello World from app.js! \nChange this message, and make sure it changes in the browser \nto verify that you're working in the right files.");
+
+var navigation = [{
+	name: "About Us",
+	link: "about.html"
+}, {
+	name: "Our Services",
+	link: "services.html"
+}, {
+	name: "Testimonials",
+	link: "testimonials.html"
+}, {
+	name: "Contact",
+	link: "contact.html"
+}];
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 console.log(`Hello World from app.js! 
 Change this message, and make sure it changes in the browser 
 to verify that you're working in the right files.`)
+
+let navigation = [
+	{
+		name: "About Us",
+		link: "about.html"
+	},
+	{
+		name: "Our Services",
+		link: "services.html"
+	},
+	{
+		name: "Testimonials",
+		link: "testimonials.html"
+	},
+	{
+		name: "Contact",
+		link: "contact.html"
+	},
+]
